e1f662908c931a89fcd8a1d22b7e6665fa25a3a3
João Vitor <joaovitorpd@gmail.com>
2022-09-19 09:41:57 -0300

    Updated library at weather_repository.dart

--- lib/domain/repositories/weather_repository.dart
+++ lib/domain/repositories/weather_repository.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:weather_tdd_flutter/domain/entities/weather.dart';
+import '../../data/failure.dart';
 
 abstract class WeatherRepository {
   Future<Either<Failure, Weather>> getCurrentWeather(String cityName);
