6ba23cd4851f7452a30f04bb4f6ba5f8ae63be62
João Vitor <joaovitorpd@gmail.com>
2022-09-19 10:23:17 -0300

    Implemented test_helper.dart

--- test/helpers/test_helper.dart
+++ test/helpers/test_helper.dart
@@ -1,11 +1,9 @@
-import 'package:flutter_weather_app_sample/data/datasources/remote_data_source.dart';
-import 'package:flutter_weather_app_sample/domain/repositories/weather_repository.dart';
 import 'package:mockito/annotations.dart';
 import 'package:http/http.dart' as http;
+import 'package:weather_tdd_flutter/domain/repositories/weather_repository.dart';
 
 @GenerateMocks([
   WeatherRepository,
-  RemoteDataSource,
 ], customMocks: [
   MockSpec<http.Client>(as: #MockHttpClient)
 ])
