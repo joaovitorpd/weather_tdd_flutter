fb1d87f8818b0e740574dfac4958e2fc4a497274
João Vitor <joaovitorpd@gmail.com>
2022-09-19 09:44:13 -0300

    Coded get_current_weather.dart

--- lib/domain/usecases/get_current_weather.dart
+++ lib/domain/usecases/get_current_weather.dart
@@ -1,7 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:weather_tdd_flutter/data/failure.dart';
+import 'package:weather_tdd_flutter/domain/entities/weather.dart';
 import 'package:weather_tdd_flutter/domain/repositories/weather_repository.dart';
 
 class GetCurrentWeather {
   final WeatherRepository repository;
 
-  GetCurrentWeather()
+  GetCurrentWeather(this.repository);
+
+  Future<Either<Failure, Weather>> execute(String cityName) {
+    return repository.getCurrentWeather(cityName);
+  }
 }
